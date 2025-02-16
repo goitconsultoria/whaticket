b7dc549a7e727caee139ad6c180289a931d067d5
goitconsultoria <rafael.benevides@goit.com.br>
2025-02-16 19:00:07 -0300

    a

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -11,7 +11,7 @@ get_mysql_root_password() {
 get_link_git() {
   
   print_banner
-  printf "${WHITE} 💻 Insira o link do GITHUB do seu Canal Vem Fazer que deseja instalar:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Insira o link do GITHUB do Whaticket que deseja instalar:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " link_git
 }
@@ -201,14 +201,14 @@ software_dominio() {
 inquiry_options() {
   
   print_banner
-  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador Canal Vem Fazer, selecione abaixo a proxima ação!${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador Whaticket, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
   printf "\n\n"
-  printf "   [0] Instalar Canal Vem Fazer\n"
-  printf "   [1] Atualizar Canal Vem FazerP\n"
-  printf "   [2] Deletar Canal Vem Fazer\n"
-  printf "   [3] Bloquear Canal Vem FazerP\n"
-  printf "   [4] Desbloquear Canal Vem Fazer\n"
-  printf "   [5] Alter. dominio Canal Vem Fazer\n"
+  printf "   [0] Instalar whaticket\n"
+  printf "   [1] Atualizar whaticket\n"
+  printf "   [2] Deletar Whaticket\n"
+  printf "   [3] Bloquear Whaticket\n"
+  printf "   [4] Desbloquear Whaticket\n"
+  printf "   [5] Alter. dominio Whaticket\n"
   printf "\n"
   read -p "> " option
 
--- utils/_banner.sh
+++ utils/_banner.sh
@@ -21,18 +21,25 @@ print_banner() {
 
   printf "\n\n"
 
-printf "${CYAN_LIGHT}";
-
-
-printf ${CYAN_LIGHT}" ____   ____  ________  ____    ____   ________    _       ________  ________  _______   \n";  
-printf ${CYAN_LIGHT}"|_  _| |_  _||_   __  ||_   \  /   _| |_   __  |  / \     |  __   _||_   __  ||_   __ \  \n"; 
-printf ${CYAN_LIGHT}"  \ \   / /    | |_ \_|  |   \/   |     | |_ \_| / _ \    |_/  / /    | |_ \_|  | |__) | \n";
-printf ${CYAN_LIGHT}"   \ \ / /     |  _| _   | |\  /| |     |  _|   / ___ \      .'.' _   |  _| _   |  __ /  \n";
-printf ${CYAN_LIGHT}"    \ ' /     _| |__/ | _| |_\/_| |_   _| |_  _/ /   \ \_  _/ /__/ | _| |__/ | _| |  \ \_\n";
-printf ${CYAN_LIGHT}"     \_/     |________||_____||_____| |_____||____| |____||________||________||____| |___|\n";
-                                                                                                                                                         
-  printf "            \033[1;33m        © CANAL VEM FAZER - https://www.youtube.com/channel/UCwFO9ylM7gHxYIXfJqAo1vQ\n";
+  printf "${GREEN}";
+  printf "                                                     ▄▄█▀▀▀▀▀▀▀█▄▄  \n";
+  printf "                                                   ${GREEN}▄█▀${NC}   ${WHITE}▄▄${NC}      ${GREEN}▀█▄\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}███${NC}         ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}██▄         ${GREEN}█${NC}\n";
+  printf "                                                   ${GREEN}█${NC}     ${WHITE}▀██▄${NC} ${WHITE}██${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}       ${WHITE}▀███▀${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}▀█▄           ▄█▀\n";
+  printf "                                                    ▄█    ▄▄▄▄█▀▀  \n";
+  printf "                                                    █  ▄█▀        \n";
+  printf "                                                    ▀▀▀▀          \n";
   printf "${NC}";
+
+  printf "\n"
+
+printf "${GREEN}";  
+printf "${NC}";
+  
+  
 
   printf "\n"
 }
